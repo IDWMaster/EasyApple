fc0b01a89bdcb2faa20797edf03d7f9e5ec996dc
Brian Bosak <webadm@elcnet.servehttp.com>
2014-11-23 16:21:07 -0600

    Initial Commit

---
+++ EasyApple/EasyApple.h
@@ -0,0 +1,13 @@
+//
+//  EasyApple.h
+//  EasyApple
+//
+//  Created by Brian Bosak on 11/23/14.
+//  Copyright (c) 2014 Brian Bosak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EasyApple : NSObject
+
+@end
---
+++ EasyApple/EasyApple.m
@@ -0,0 +1,13 @@
+//
+//  EasyApple.m
+//  EasyApple
+//
+//  Created by Brian Bosak on 11/23/14.
+//  Copyright (c) 2014 Brian Bosak. All rights reserved.
+//
+
+#import "EasyApple.h"
+
+@implementation EasyApple
+
+@end
